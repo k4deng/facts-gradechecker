e5314d43e4b7ed47ebd178992d6bc4fafc2dc72f
k4deng <70725766+k4deng@users.noreply.github.com>
2023-10-05 22:34:51 -0400

    (feat.) homework grid

--- routes/site.js
+++ routes/site.js
@@ -6,12 +6,16 @@ const config = require("../config.js");
 
 // API
 const factsapi = require("../modules/factsapi/factsapi.js");
+const { makeAuthRequest } = require("../modules/factsapi/reqhelper.js");
 
 // database
 const { database, authdb } = require("../modules/enmap.js");
 
 // updater
 const updater = require("../modules/updater.js");
+
+// moment for times
+const moment = require("moment");
 
 // Express Session
 const express = require("express");
@@ -83,6 +87,57 @@ module.exports = function() {
         logs: info.result
       });
 
+  });
+
+  // homework grid 
+  router.get("/homework", async (req, res) => {
+
+    const ssInfo = await makeAuthRequest("https://nbsmobileapi.renweb.com/api/SchoolsAndStudents/v1?featureName=10");
+    const defaultTerm =
+        config.defaultTerm !== 0 //if config default term exists
+          ? config.defaultTerm  //use it
+          : ssInfo.defaultTermId; //else grab school default term
+  
+    const rawHWData = await makeAuthRequest(`https://nbsmobileapi.renweb.com/api/Homework/students/${ssInfo.defaultStudentId}/schoolYearId/${ssInfo.defaultYearId}/legacyTermId/${defaultTerm}`);
+  
+    let formattedHWData = {};
+  
+    const selectedDate = moment();
+    //get the homework within the selected week range
+    for (const timeRange of rawHWData.studentHomeworkDateRangeModels) {
+
+      //skip if not current week
+      const dateRangeStart = moment(timeRange.dateRangeStart);
+      const dateRangeEnd = moment(timeRange.dateRangeEnd);
+      if (!selectedDate.isBetween(dateRangeStart, dateRangeEnd)) continue;
+
+      // sort hw by class
+      const classIds = [...new Set(timeRange.notesDetails.map(item => item.classId))];
+      classIds.sort((a, b) => a - b);
+
+      const sortedData = {};
+      for (const classId of classIds) {
+        // Filter the notesDetails for the current class
+        const classNotes = timeRange.notesDetails.filter(item => item.classId === classId);
+
+        // Sort the classNotes by eventDate (day)
+        classNotes.sort((a, b) => a.eventDate.localeCompare(b.eventDate));
+
+        // Store the sorted classNotes in the sortedData object
+        sortedData[classId] = classNotes;
+      }
+      formattedHWData = sortedData;
+
+    }
+
+    // give json result if asked for
+    if (req.query.json) return res.json(formattedHWData);
+
+    // otherwise html
+    res.render(path.resolve(`${process.cwd()}/views/homework.ejs`), {
+      moment: moment,
+      hwData: formattedHWData
+    });
   });
 
   // updates
